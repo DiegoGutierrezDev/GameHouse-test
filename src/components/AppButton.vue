c6f897801a35eb3030dbe2805fd554bd7983664f
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-06-12 14:15:32 +0200

    Button component

---
+++ src/components/AppButton.vue
@@ -0,0 +1,35 @@
+<script setup>
+const props = defineProps({
+  text: String,
+  theme: {
+    type: String,
+    default: 'LOrange',
+  },
+})
+</script>
+
+<template>
+  <button :class="props.theme">
+    {{ props.text }}
+  </button>
+</template>
+
+<style scoped>
+.Mpurple {
+  background-color: #c2b4f7;
+  width: 10em;
+  height: 3em;
+  border-radius: 0.5rem;
+  font-weight: 600;
+  font-size: large;
+}
+
+.LOrange {
+  background-color: #fccd61;
+  width: 18em;
+  height: 3em;
+  border-radius: 0.5rem;
+  font-weight: 600;
+  font-size: large;
+}
+</style>
