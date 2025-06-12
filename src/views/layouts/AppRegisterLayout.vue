66b636fb5c2ef99a1866a5fa5aa87ca9c7f0439c
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-06-12 13:57:16 +0200

    Structure based in layouts

---
+++ src/views/layouts/AppRegisterLayout.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue'
+
+const activeStep = ref('step' + '2')
+</script>
+
+<template>
+  <component :is="activeStep">
+    <slot />
+  </component>
+</template>
