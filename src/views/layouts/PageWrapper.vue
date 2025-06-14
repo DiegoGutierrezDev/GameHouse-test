23c65fcf9314222a949abe27b3ced16fdfffaa49
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-06-14 20:19:09 +0200

    Pseudolayouts created

---
+++ src/views/layouts/PageWrapper.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="desktop" class="columns">
+    <div>
+      <slot name="benefits" />
+      <slot name="text" />
+    </div>
+    <div class="white-text">
+      <slot name="title" />
+      <slot name="input" />
+      <slot name="resend" />
+      <slot name="button" />
+    </div>
+  </div>
+  <div v-else>
+    <slot name="title" />
+    <slot name="benefits" />
+    <slot name="input" />
+    <slot name="button" />
+    <slot name="text" />
+  </div>
+</template>
+
+<script setup>
+import { computed, onMounted, onUnmounted, ref } from 'vue'
+
+const windowWidth = ref(window.innerWidth)
+const updateWidth = () => (windowWidth.value = window.innerWidth)
+
+onMounted(() => window.addEventListener('resize', updateWidth))
+onUnmounted(() => window.removeEventListener('resize', updateWidth))
+
+const desktop = computed(() => {
+  return windowWidth.value > 641
+})
+</script>
--- src/views/register/RegisterStep2.vue
+++ src/views/register/RegisterStep2.vue
@@ -2,32 +2,41 @@
 import AppBenefits from '@/components/AppBenefits.vue'
 import AppButton from '@/components/AppButton.vue'
 import AppOTP from '@/components/AppOTP.vue'
+import PageWrapper from '../layouts/PageWrapper.vue'
 </script>
 
 <template>
-  <main class="columns">
-    <div class="left-column">
-      <AppBenefits
-        :benefits="[
-          'Access to 10+ GAMES for FREE thanks to ads',
-          'Log In Across All Your Devices',
-          'Skip the Line with Customer Support',
-        ]"
-      />
-    </div>
+  <PageWrapper>
+    <template #benefits>
+      <div class="left-column">
+        <AppBenefits
+          :benefits="[
+            'Access to 10+ GAMES for FREE thanks to ads',
+            'Log In Across All Your Devices',
+            'Skip the Line with Customer Support',
+          ]"
+        />
+      </div>
+    </template>
 
-    <div class="white-text">
+    <template #input>
+      <AppOTP style="margin-top: 1.4em"></AppOTP>
+    </template>
+
+    <template #title>
       <h1>Get verified!</h1>
       <h3>Enter the one-time code we sent to:</h3>
       EMAIL
-      <AppOTP style="margin-top: 1.4em"></AppOTP>
+    </template>
 
+    <template #resend>
       <p style="margin-top: 3em">
         Didn't get an email?
         <a href="https://company.gamehouse.com/" style="font-weight: 700">Resend Code</a>
-      </p>
-
+      </p></template
+    >
+    <template #button>
       <AppButton text="Verify" style="display: block; width: 100%; margin-top: 1em"></AppButton>
-    </div>
-  </main>
+    </template>
+  </PageWrapper>
 </template>
