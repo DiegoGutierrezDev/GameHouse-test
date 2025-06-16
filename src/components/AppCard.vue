3e60b97953fc1e663dbc21927862620ca0878607
Diego Gutierrez <gutierrezmaria.diego@gmail.com>
2025-06-16 12:56:11 +0200

    Little visual fix

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -9,7 +9,12 @@ const selected = ref('year')
 </script>
 
 <template>
-  <div v-for="(product, index) in products" class="plan-card" :key="index">
+  <div
+    v-for="(product, index) in products"
+    class="plan-card"
+    :key="index"
+    :class="{ 'selected-border ': selected == index }"
+  >
     <div v-if="index == 'year'" class="badge">Save 20%</div>
     <div class="card-header">
       <input type="radio" class="checkbox-round" name="card" v-model="selected" :value="index" />
@@ -28,33 +33,46 @@ const selected = ref('year')
 </template>
 
 <style scoped>
+.selected-border {
+  border-width: 3px;
+  border-style: solid;
+  border-radius: 1em;
+  border-image: linear-gradient(90deg, #ffc978, #fd6a6a, #b991c0, #7ed6df) 1;
+}
 .plan-card {
+  display: grid;
+  margin-left: 1em;
+  margin-right: 1em;
+  grid-template-columns: repeat(2, 1fr);
   position: relative;
   background: linear-gradient(to right, #2f234d, #3c2e72);
-  border-radius: 1em;
-  border: 3px solid transparent;
   text-align: center;
   color: white;
-  font-family: 'Segoe UI', sans-serif;
   box-shadow: 0 6px 16px rgba(0, 0, 0, 0.3);
-  background-clip: padding-box;
-  border-image: linear-gradient(90deg, #ffc978, #fd6a6a, #b991c0, #7ed6df) 1;
+}
+.card-header {
+  padding-left: 0.4em;
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  padding-right: 0.4em;
+  background-color: #5a4a94;
+  display: grid;
+  justify-content: start;
+  align-items: center;
+}
+@media (min-width: 641px) {
+  .plan-card {
+    display: block;
+  }
+
+  .card-header {
+    grid-template-columns: repeat(4, minmax(0, 1fr));
+  }
 }
 
 .price-container {
   margin-top: 12px;
   padding-left: 5.8em;
   padding-right: 5.8em;
-}
-
-.card-header {
-  grid-template-columns: repeat(4, minmax(0, 1fr));
-  padding-left: 0.4em;
-  padding-right: 0.4em;
-  background-color: #5a4a94;
-  display: grid;
-  justify-content: start;
-  align-items: center;
 }
 
 .white-text {
